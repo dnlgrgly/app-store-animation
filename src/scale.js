1dbe63778412db74fa24ee445e5e4508cbb26b8e
Daniel <dandesz198@gmail.com>
2018-08-05 19:25:36 +0200

    Unused const delete

--- src/scale.js
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from 'react-native';
-
-const { height, width } = Dimensions.get('window');
-
-const guidelineBaseWidth = 350;
-
-export const scale = size => (width / guidelineBaseWidth) * size;
-export const cardHeight = scale(height - 200);
